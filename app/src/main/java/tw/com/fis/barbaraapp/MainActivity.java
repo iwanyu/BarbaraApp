62991dbde12e67c3bdf450ae067297f6de231c9f
f100057 <barbara1115@gmail.com>
2015-04-20 17:03:01 +0800

    test

--- app/src/main/java/tw/com/fis/barbaraapp/MainActivity.java
+++ app/src/main/java/tw/com/fis/barbaraapp/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement_Barbara
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
